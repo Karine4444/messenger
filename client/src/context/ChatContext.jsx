bd5c2029f8be2f47b82cc3e17fad64d27db4e22e
anushmanvelyan <anush.manvelyan935@gmail.com>
2024-12-14 19:06:46 +0400

    Adding Socket folder

---
+++ client/src/context/ChatContext.jsx
@@ -0,0 +1,144 @@
+import { useCallback } from "react";
+import { createContext, useState, useEffect } from "react";
+import { baseUrl, getRequest, postRaequest } from "../utils/services";
+import { io } from "socket.io-client";
+
+export const ChatContext = createContext();
+
+export const ChatContextProvider = ({children, user }) => {
+  const [userChats, setUserChats] = useState(null);
+  const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
+  const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState(null);
+  const [isMessagesLoading, setIsMessagesLoading] = useState(false);
+  const [messagesError, setMessagesError] = useState(null);
+  const [sendTextMessageError, setSendTextMessageError] = useState(null);
+  const [newMessage, setNewMessage] = useState(null);
+  const [socket, setSocket] = useState(null);
+  console.log("messages", messages);
+
+  // initial socket
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+    setSocket(newSocket);
+    return () => {
+      newSocket.disconnect();
+    };
+  }, [user]);
+
+  // add online users
+  useEffect(() => {
+    if (socket === null) return;
+    socket.emit("addNewUser", user?._id);
+    socket.on("getOnlineUsers", (res) => {
+      setOnlineUsers(res);
+    });
+    return () => {
+      socket.off("getOnlineUsers");
+    };
+  }, [socket]);
+  // send message
+  useEffect(() => {
+    if (socket === null) return;
+    const recipientId = currentChat?.members?.find((id) => id !== user?._id);
+    socket.emit("sendMessage", { ...newMessage, recipientId });
+  }, [newMessage]);
+
+  // receive message
+  useEffect(() => {
+    if (socket === null) return;
+    socket.on("getMessage", (res) => {
+      if (currentChat?._id !== res.chatId) return;
+      setMessages((prev) => [...prev, res]);
+    });
+    return () => {
+      socket.off("getMessage");
+    };
+  }, [socket, currentChat]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`$(baseurl}/users`);
+      if (response.error) {
+        return console.log("Error fetching users", response);
+      }
+      const pChats = response?.filter((u) => {
+        let isChatCreated = false;
+        if (user?._id === u._id) return false;
+        if (userChats) {
+          isChatCreated = userchats?.some((chat) => {
+            return chat.members[0] === u._id || chat.members[1] === u._id;
+          });
+        }
+        return !isChatCreated;
+      });
+      setPotentialChats(pChats);
+    };
+    getUsers();
+  }, [userChats]);
+  useEffect(() => {
+    const getUserChats = async () => {
+      if (user?._id) {
+        setIsUserChatsLoading(true);
+        setUserChatsError(null);
+        const response = await getRequest(`${baseUrl}/chats/${user?.id}`);
+        setIsUserChatsLoading(false);
+
+        if (response.error) {
+          return setUserChatsError(response);
+        }
+        setUserChats(response);
+      }
+    };
+    getUserChats();
+
+  }, [user]);
+
+  //127-150
+  const sendTextMessage = useCallback(
+    async (textMessage, sender, currentChatId, setTextMessage) => {
+      if (!textMessage) return console.log("You must type something...");
+      console.log("sender", sender);
+      const response = await postRequest(
+        `${baseUrl}/messages`,
+        JSON.stringify({
+          chatId: currentChatId,
+          senderId: sender._id,
+          text: textMessage,
+        })
+      );
+      if (response.error) {
+        return setSendTextMessageError(response);
+      }
+      setNewMessage(response);
+      setMessages((prev) => [...prev, response]);
+      setTextMessage(" ");
+    },
+  []
+};
+
+// 158
+return {
+    <ChatContext.Provider
+    value={{ 
+    userChats,
+    isUserChatsLoading,
+    userChatsError, 
+    potentialChats, 
+    createChat, 
+    updateCurrentChat, 
+    messages, 
+    isMessagesLoading,
+    messagesError, 
+    currentchat,
+    sendTextMessage,
+    onlinUsers,
+    }}
+    >
+    {children}
+    </ChatContext.Provider>
+};
+};
